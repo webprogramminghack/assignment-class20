0d2971c19aa5448c682ebfd855e5a34967b04075
RudyHarun99 <rdyhrn@gmail.com>
2024-11-07 21:08:02 +0700

    progress 1: usePrevious

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,1 +1,12 @@
 // implement the usePrevious hook here
+import { useRef, useEffect } from "react";
+
+export const usePrevious = (value: number = 0): number => {
+  const prevCountRef = useRef<number>(value);
+
+  useEffect(() => {
+    prevCountRef.current = value;
+  }, [value]);
+
+  return prevCountRef.current;
+}
